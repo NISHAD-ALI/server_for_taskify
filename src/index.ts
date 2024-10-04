1f7ec7aa6bd574233b10402162c406ccf7a8dcf2
nishad <nishadalichenadan@gmail.com>
2024-10-04 22:37:47 +0530

    first-final

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,22 @@ dotenv.config();
 
 const app = express();
 
-// Database connection
+
 connectDB();
 
-// Middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// CORS configuration
 app.use(cors({ 
-  origin: 'http://localhost:5173', // Ensure no trailing slash
+  origin: 'https://taskifyco.vercel.app', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true // Allow credentials (cookies, authorization headers, etc.)
+  credentials: true 
 }));
 
-// Routes
+
 app.use('/', router); 
 
-// Start server
 app.listen(4000, () => {
   console.log('Server is running on http://localhost:4000');
 });
